0e0d966f1bd69e0b56aa953a1409034523de910d
Brian Faust <hello@brianfaust.me>
2018-02-03 16:25:20 +0200

    update tests
    Signed-off-by: Brian Faust <hello@brianfaust.me>

--- test/transaction_test.exs
+++ test/transaction_test.exs
@@ -3,6 +3,7 @@ defmodule ArkElixir.TransactionTest do
   import ArkElixir.Transaction
 
   @client ArkElixir.Client.new(%{
+            protocol: "https",
             ip: "127.0.0.1",
             port: 4002,
             nethash: "578e820911f24e039733b45e4882b73e301f813a0d2c31330dafda84534ffa23",
--- test/vote_test.exs
+++ test/vote_test.exs
@@ -3,6 +3,7 @@ defmodule ArkElixir.VoteTest do
   import ArkElixir.Vote
 
   @client ArkElixir.Client.new(%{
+            protocol: "https",
             ip: "127.0.0.1",
             port: 4002,
             nethash: "578e820911f24e039733b45e4882b73e301f813a0d2c31330dafda84534ffa23",
